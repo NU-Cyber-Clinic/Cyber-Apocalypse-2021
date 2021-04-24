a979bd40ef883d52a55d15e79451a9996e09088d
cosmin-bianu <54325599+cosmin-bianu@users.noreply.github.com>
2021-04-24 02:00:14 +0100

    Added PhaseStream 2

--- crypto/PhaseStream 2/decrypt.py
+++ crypto/PhaseStream 2/decrypt.py
@@ -1,3 +1,5 @@
+#Python 2.7
+
 def sxor(s1,s2):    
     # convert strings to a list of character pair tuples
     # go through each tuple, converting them to ASCII code (ord)
@@ -8,16 +10,14 @@ def sxor(s1,s2):
 
 cipher_file = open('output.txt')
 
-
-
 counter = 0
 for cipher in cipher_file:
     for i in range(256):
         key = chr(i)*55
-        decoded_cipher = bytearray.fromhex(cipher).decode('ascii')
+        decoded_cipher = cipher.lstrip().rstrip().decode("hex")
         plain = sxor(decoded_cipher, key)
         if 'CHTB{' in plain:
             print(plain)
     counter += 1
     if counter % 1000 == 0:
-        print('Passed line ' + str(counter))
+        print "Passed line " + str(counter)
